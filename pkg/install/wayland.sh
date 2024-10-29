d27b8eca484673d10697e4ac867d0f5ff9419302
Pettttter1 <982961222@qq.com>
2024-10-29 06:21:34 +0000

    add docker gnome wayland install script

---
+++ pkg/install/wayland.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ ! -d $1 ]
+then
+       mkdir -p $1
+fi
+
+cd $1
+repos=(https://gitlab.freedesktop.org/wayland/wayland.git
+       https://gitlab.freedesktop.org/wayland/wayland-protocols.git
+       https://gitlab.freedesktop.org/wayland/weston.git)
+
+for repo in ${repos[@]}
+do
+        git clone $repo
+done
+
+cd -
+# for wayland
+sudo apt install pkg-config libxml2-dev graphviz doxygen xsltproc xmlto \
+ninja-build expat libffi-dev libwayland-dev
